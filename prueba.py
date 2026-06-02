bd9d5e512e39d49b405b1001b11a8ab547872121
Fidel <fidel.cr92@gmail.com>
2026-06-02 09:34:10 -0400

    modificacion de la segunda rama

--- prueba.py
+++ prueba.py
@@ -1,8 +1,3 @@
-def suma(a, b, c=0, d=0):
-	if c != 0 and d != 0:
-		return a + b + c + d
-	elif c != 0:
-		return a + b + c
-	else:
-		return a + b
-print(suma(2,2,2))  
+def suma(*num):
+	return sum()
+print(suma(2,2))
